c69f69337480ce19365af3599361e94c88d2583f
Itzhak Efraimov <itzhakefraimov@gmail.com>
2020-07-10 21:41:00 +0300

    Update and rename 03082017-B-2A.asm to 03082017-B-2.asm

--- 03082017-B-2.asm
+++ 03082017-B-2.asm
@@ -1,5 +1,7 @@
 ; 03.08.2017 - Moed B - Question 2/A
 .model small ; large
+.data
+	max dw ?
 .code
 .386
 ; extern int compare_arrs(long int arr1[], long int arr2[], int m);
@@ -58,4 +60,55 @@ _compare_arrs proc near ; far
 	pop bp
 	ret
 _compare_arrs endp
+
+; extern int arr_max(long int *arr_ptr[], int n, int m);
+;							[BP+4]		  [BP+6] [BP+8]
+public _arr_max
+_arr_max proc near
+	push bp
+	mov bp,sp
+	
+	push di
+
+	mov di,[bp+4] ; di = arr_ptr
+	mov cx,[bp+6] ; cx = n
+	mov max,0
+	
+	cmp cx,1 ; if(n <= 1) => return 0
+	jle end2
+	
+	xor bx,bx
+	cmp word ptr [bp+8],bx ; if(m <= 0) => return 0
+	jle end2
+	
+	l2:
+	mov bx,word ptr [bp+4]
+	mov ax,max
+	mov dx,2
+	mul dx
+	add bx,ax
+	push cx ; saves cx value because compare_arrs also uses cx
+	push word ptr [bp+8] ; m
+	push word ptr [di] ; arr[i]
+	push word ptr [bx] ; arr[max]
+	call _compare_arrs
+	add sp,6
+	pop cx
+	
+	cmp ax,0
+	jge continue
+	mov dx,word ptr [bp+8]
+	sub dx,cx
+	mov max,dx
+	
+	continue: 
+	add di,2
+	loop l2
+	
+	end2:
+	mov ax,max
+	pop di
+	pop bp
+	ret
+_arr_max endp
 end
